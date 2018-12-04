84b86177252bbb71ca2af1571eb41182dd0b1a06
Damian Rolewski <damianrolewski@gmail.com>
2018-12-04 17:35:43 +0100

    Funkcja aparatu
    Do aplikacji dodano możliwość użycia wbudowanego w urządzenie mobilne aparatu.

---
+++ app/src/main/java/damianrolewsk/com/detektortwarzy/MainActivity.java
@@ -0,0 +1,62 @@
+package damianrolewsk.com.detektortwarzy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+
+    Button button;
+    ImageView imageView;
+    static final int CAM_REQUEST =1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.button);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent, CAM_REQUEST);
+
+            }
+        });
+    }
+
+
+    private File getFile(){
+        File folder = new File("sdcard/camera_app");
+
+        if(!folder.exists()){
+            folder.mkdir();
+        }
+
+
+        File image_file = new File(folder, "cam_image.jpg");
+
+
+
+
+        return image_file;
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+    }
+}
