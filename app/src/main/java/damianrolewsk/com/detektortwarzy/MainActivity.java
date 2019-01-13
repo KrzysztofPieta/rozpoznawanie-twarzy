4af53d42fb9328aba0353bcda7445e7a0d7f482d
Damian Rolewski <damianrolewski@gmail.com>
2019-01-13 10:41:40 +0100

    w pelni dzialajaca aplikacja, dzialaja oba przyciki i wykrywaja twarze

--- app/src/main/java/damianrolewsk/com/detektortwarzy/MainActivity.java
+++ app/src/main/java/damianrolewsk/com/detektortwarzy/MainActivity.java
@@ -10,6 +10,8 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
@@ -17,9 +19,14 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
 
 import java.io.File;
 
@@ -33,11 +40,20 @@ public class MainActivity extends AppCompatActivity {
     ImageView obraz;
     Button button;
     Button camera;
+    Bitmap myBitmap;
+    Paint myRectPaint = new Paint();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*ramka*/
+
+        myRectPaint.setStrokeWidth(5);
+        myRectPaint.setColor(Color.RED);
+        myRectPaint.setStyle(Paint.Style.STROKE);
 
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST );
@@ -103,14 +119,68 @@ public class MainActivity extends AppCompatActivity {
                     String picturePath = cursor.getString(columnIndex);
                     cursor.close();
                     obraz.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    myBitmap = BitmapFactory.decodeFile(picturePath);
+
+
+
+
+
+                    Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+                    Canvas tempCanvas = new Canvas(tempBitmap);
+                    tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+                    FaceDetector faceDetector = new
+                            FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                            .build();
+
+                    Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                    SparseArray<Face> faces = faceDetector.detect(frame);
+
+                    for(int i=0; i<faces.size(); i++) {
+                        Face thisFace = faces.valueAt(i);
+                        float x1 = thisFace.getPosition().x;
+                        float y1 = thisFace.getPosition().y;
+                        float x2 = x1 + thisFace.getWidth();
+                        float y2 = y1 + thisFace.getHeight();
+                        tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+                    }
+                    obraz.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
 
                     break;
                 }
 
-                /*to trzeba usunac zeby dzialalo z galerii*/
+                /*wykrywanie z aparatu*/
             case CAM_REQUEST:
-                String path = "sdcard/camera_app/cam_image.jpg";
-                obraz.setImageDrawable(Drawable.createFromPath(path));
+                String path = "sdcard/camera_app/cam_image.jpg"; /* sciezka do zrobionego zdjecia*/
+
+                myBitmap = BitmapFactory.decodeFile(path); /* przetworzenie sciezki na mape bitowa*/
+
+                Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565); /*pobieranie parametrow obrazu*/
+                Canvas tempCanvas = new Canvas(tempBitmap);
+                tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+                /*tworzenie obiektu facedetectora*/
+                FaceDetector faceDetector = new
+                        FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                        .build();
+
+                Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+                SparseArray<Face> faces = faceDetector.detect(frame);
+
+                /*wylapywanie wspolrzednych twarzy i przypisywanie ich ramce*/
+                for(int i=0; i<faces.size(); i++) {
+                    Face thisFace = faces.valueAt(i);
+                    float x1 = thisFace.getPosition().x;
+                    float y1 = thisFace.getPosition().y;
+                    float x2 = x1 + thisFace.getWidth();
+                    float y2 = y1 + thisFace.getHeight();
+                    tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+                }
+                /*ustawienie obrazy z ramka na nasz imageView*/
+                obraz.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+
+
+
                 break;
 
             default :
